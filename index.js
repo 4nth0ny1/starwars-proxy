d9c244dd4362235eee8f6359becdff68157e66e0
4nth0ny1 <acatullo4@gmail.com>
2022-02-09 18:51:18 -0600

    add middleware and needle requirements and adjusted routes for api request

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const api_helper = require('./API_helper')
 const app = express()
+const apicache = require('apicache')
+const needle = require('needle')
 const port = 3000
 
-/*
-* Route to DEMO the API call to a REST API Endpoint 
-* REST URL : https://jsonplaceholder.typicode.com/todos/1
-*/
-app.get('/getAPIResponse', (req, res) => {
-    api_helper.make_API_call('https://swapi.dev/api/people/1')
+// app.get('/getAPIResponse', (req, res) => {
+//     api_helper.API_call('https://swapi.dev/api/people/1')
+//     .then(response => {
+//         res.json(response)
+//     })
+//     .catch(error => {
+//         res.send(error)
+//     })
+// })
+
+let cache = apicache.middleware
+
+app.get('/', cache('2 minutes'), async (req, res) => {
+    api_helper.API_call('https://swapi.dev/api/')
     .then(response => {
         res.json(response)
     })
