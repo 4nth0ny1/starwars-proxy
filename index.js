41f7ffd80c4a130d233dfde4fd4e76a09780bf0b
4nth0ny1 <acatullo4@gmail.com>
2022-02-09 19:00:48 -0600

    removed apihelper and am using needle with apicache

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const express = require('express')
-const api_helper = require('./API_helper')
 const app = express()
 const apicache = require('apicache')
 const needle = require('needle')
 const port = 3000
 
-// app.get('/getAPIResponse', (req, res) => {
-//     api_helper.API_call('https://swapi.dev/api/people/1')
-//     .then(response => {
-//         res.json(response)
-//     })
-//     .catch(error => {
-//         res.send(error)
-//     })
-// })
-
 let cache = apicache.middleware
 
 app.get('/', cache('2 minutes'), async (req, res) => {
-    api_helper.API_call('https://swapi.dev/api/')
-    .then(response => {
-        res.json(response)
-    })
-    .catch(error => {
+    try {
+        const apiRes = await needle('get', 'https://swapi.dev/api/people/3')
+        const data = apiRes.body
+        console.log(data)
+        res.status(200).json(data)
+    } catch (error) {
         res.send(error)
-    })
+    }
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
