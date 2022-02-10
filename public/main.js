007162ddf23d7cc8ebb6f970dec280bacd3bf54d
4nth0ny1 <acatullo4@gmail.com>
2022-02-09 19:20:58 -0600

    added frontend

---
+++ public/main.js
@@ -0,0 +1,41 @@
+const characterDisplay = document.querySelector('.character');
+const characterSelectForm = document.querySelector('.character-select-form');
+const characterInput = document.querySelector('#character-input');
+
+// fetch character data from api
+const fetchCharacter = async (char) => {
+    const url = `/?search=${char}`
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    const displayData = {
+        characterName: data.name,
+        characterHeight: data.height,
+      }
+    
+      addCharacterToDOM(displayData)
+}
+
+
+// Add display data to DOM
+const addCharacterToDOM = (data) => {
+    characterDisplay.innerHTML = `
+      <h1>${data.name}</h1>
+      <h2>${data.height} &deg;F</h2>
+    `
+    characterInput.value = ''
+  }
+
+  // Event listener for form submission
+characterSelectForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+  
+    if (characterInput.value === '') {
+      alert('Please enter a famous Star Wars character')
+    } else {
+      fetchCharacter(characterInput.value)
+    }
+  })
+
+  fetchCharacter('Luke')
